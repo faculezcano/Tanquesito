a5f57a5ccda321854f5aae8e9314eb0b07ce4777
faculezcano <facu_lezca@hotmail.com>
2016-09-01 20:26:56 -0300

    Se le pone un sprite de prueba

--- proto/src/Objetos/Ladrillo.java
+++ proto/src/Objetos/Ladrillo.java
@@ -2,15 +2,22 @@ package Objetos;
 import Sistema.ObjetoEstatico;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class Ladrillo extends ObjetoEstatico{
 	
-	protected Rectangle rect1 = new Rectangle(0,0,32,32);
+	protected Rectangle rect1;
 	
 	public Ladrillo(){
-		
+		rect1 = new Rectangle(0,0,32,32);
+		rect1.setFill(new ImagePattern(new Image(getClass().getClassLoader().getResourceAsStream("img/ladrillo.png"))));
+		DropShadow ds = new DropShadow();
+		ds.setRadius(10);
+		//rect1.setEffect(ds);
 	}
 
 	@Override
