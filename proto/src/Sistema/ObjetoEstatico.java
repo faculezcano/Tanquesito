67d1cf9ec5f7be596f7e3b0c89a4dd745e6165a6
faculezcano <facu_lezca@hotmail.com>
2016-09-01 16:22:06 -0300

    Se agrega getForma() para pedir el shape del objeto

--- proto/src/Sistema/ObjetoEstatico.java
+++ proto/src/Sistema/ObjetoEstatico.java
@@ -1,6 +1,7 @@
 package Sistema;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
+import javafx.scene.shape.Shape;
 
 
 public abstract class ObjetoEstatico {
@@ -8,5 +9,7 @@ public abstract class ObjetoEstatico {
 	public abstract void setPosition(Point2D pos);
 	
 	public abstract void addToGroup(Group g);
+	
+	public abstract Shape getForma();
 
 }
