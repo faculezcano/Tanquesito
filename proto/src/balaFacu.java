e69c22d4143cbfe077e2bf3997e24911c02381e0
Sekolo <juanfranco95@hotmail.com>
2016-09-01 11:46:13 -0300

    bala nueva hereda de del facu

---
+++ proto/src/balaFacu.java
@@ -0,0 +1,12 @@
+import javafx.geometry.Point2D;
+
+public class balaFacu extends Bala {
+
+	public balaFacu(Point2D pos, Point2D vel) {
+		super(pos, vel);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
