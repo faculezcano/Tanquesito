7257b756c2abcc673949b0d1edcdc9d987be7145
faculezcano <facu_lezca@hotmail.com>
2016-09-01 16:26:04 -0300

    Excepcion lanzada si el archivo de mapa es invalido

---
+++ proto/src/Sistema/InvalidMapException.java
@@ -0,0 +1,14 @@
+package Sistema;
+
+public class InvalidMapException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidMapException(String s){
+		super(s);
+	}
+	
+}
