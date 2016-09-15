881c95187276370f90da4c0625a51cfbea2ad0ec
faculezcano <facu_lezca@hotmail.com>
2016-09-15 03:06:26 -0300

    Se completa el constructor e implementa varios metodos

--- release/src/assets/tanques/Jugador.java
+++ release/src/assets/tanques/Jugador.java
@@ -1,7 +1,16 @@
 package assets.tanques;
 
-import javafx.geometry.Point2D;
+import java.util.LinkedList;
 
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+
+import assets.Bullet;
 import assets.Tanque;
 
 
@@ -18,10 +27,34 @@ public class Jugador extends Tanque {
      * 
      */
     public Jugador() {
-    	nivel = 1;
+    	resistencia = 3;
+        vel_mov = 4;
+        vel_disparo = 6;
+        puntos = 0;
+        bullets = new LinkedList<Bullet>();
+       	cuerpo = new Rectangle(0,0,64,64);
+        canon = new Rectangle(0,0,64,64);;
+        canonAng = 0;
+        huella = new ImagePattern(new Image(getClass().getClassLoader().getResourceAsStream("img/huella.png")));;
+        pisadas = new LinkedList<Shape>();
+        nivel = 1;
     	disparos_simul = 1;
     	vida = 3;
+    	
+    	cuerpo.setFill(new ImagePattern(new Image(getClass().getClassLoader().getResourceAsStream("img/cuerpo.png"))));
+		canon.setFill(new ImagePattern(new Image(getClass().getClassLoader().getResourceAsStream("img/canon.png"))));
+    
+		DropShadow ds = new DropShadow();
+		ds.setRadius(10);
+		cuerpo.setEffect(ds);
+		canon.setEffect(ds);
     }
+    
+    //TODO: chequear esto
+    public void addToGroup(Group g){
+		g.getChildren().add(cuerpo);
+		g.getChildren().add(canon);
+	}
 
 	@Override
 	public void romper() {
@@ -50,11 +83,11 @@ public class Jugador extends Tanque {
 		}
 		
 		pisadas(p);
-		
-		cuerpo.setTranslateX(p.getX()-cuerpo.getWidth());
-		cuerpo.setTranslateY(p.getY()-cuerpo.getHeight());
-		canon.setTranslateX(p.getX()-canon.getWidth());
-		canon.setTranslateY(p.getY()-canon.getHeight());
+
+		cuerpo.setTranslateX(p.getX()-cuerpo.getWidth()/2);
+		cuerpo.setTranslateY(p.getY()-cuerpo.getHeight()/2);
+		canon.setTranslateX(p.getX()-canon.getWidth()/2);
+		canon.setTranslateY(p.getY()-canon.getHeight()/2);
 		
 	}
 
