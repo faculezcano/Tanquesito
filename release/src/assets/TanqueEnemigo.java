388ccd583c8a022f9ea0145975636357a44519db
Facu Lezcano <facu_lezca@hotmail.com>
2016-11-06 17:24:24 -0300

    Se agrega constructor y una linea que mide el tiro al jugador

--- release/src/assets/TanqueEnemigo.java
+++ release/src/assets/TanqueEnemigo.java
@@ -2,7 +2,12 @@ package assets;
 
 import java.util.Random;
 
+import assets.tanques.Jugador;
+import common.Mapa;
 import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
 
 /**
  * 
@@ -10,6 +15,42 @@ import javafx.geometry.Point2D;
 public abstract class TanqueEnemigo extends Tanque {
 	
 	protected Random rand = new Random();
+	protected Mapa map;
+	protected int distanciaTiro = 320;
+	protected int punteria = 7; // 0 - 10
+	protected boolean tiroLimpio = true;
+	
+	//Linea para saber si tiene un tiro limpio
+	protected Line tiro = new Line(0,0,0,0);
+	
+	protected TanqueEnemigo(Mapa map){
+		this.map = map;
+		tiro.setStrokeWidth(10);
+		tiro.setVisible(false);
+	}
+	
+	@Override
+	public void apuntar(Point2D pos){
+		super.apuntar(pos);
+		tiro.setEndX(pos.getX());
+		tiro.setEndY(pos.getY());
+	}
+	
+	@Override
+	public void setPosicion(Point2D p){
+		super.setPosicion(p);
+		tiro.setStartX(p.getX());
+		tiro.setStartY(p.getY());
+	}
+	
+	public Shape getLineaTiro(){
+		return tiro;
+	}
+	
+	public void setTiroLimpio(boolean tiro){
+		tiroLimpio = tiro;
+		this.tiro.setStroke(Color.RED);
+	}
 	
 	/**
 	 * Genera una velocidad en direccion segun direccion pasada por parametro
