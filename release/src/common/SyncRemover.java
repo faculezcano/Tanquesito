13d8d5ba9a0a5e97396ea22b311516253a1ec845
Facu Lezcano <facu_lezca@hotmail.com>
2016-11-06 17:23:10 -0300

    Se agrega un constructor que utiliza removeFromGroup

--- release/src/common/SyncRemover.java
+++ release/src/common/SyncRemover.java
@@ -1,21 +1,33 @@
 package common;
 
+import assets.ObjetoDinamico;
 import javafx.scene.Group;
 import javafx.scene.shape.Shape;
 
 public class SyncRemover implements Runnable {
 	Group g;
 	Shape s;
+	ObjetoDinamico o;
+	
 	public SyncRemover(Shape s, Group g){
 		this.g = g;
 		this.s = s;
 	}
+	
+	public SyncRemover(ObjetoDinamico o, Group g){
+		this.g = g;
+		this.o = o;
+	}
 
 	@Override
 	public void run() {
-		if(g!=null && s!=null)
+		if(g!=null && s!=null){
 			if (!g.getChildren().isEmpty())
 				g.getChildren().remove(s);
+		}else if(o!=null && g!=null){
+			o.removeFromGroup(g);
+		}
+		
 	}
 
 }
