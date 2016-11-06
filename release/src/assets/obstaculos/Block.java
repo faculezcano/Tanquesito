f9983e721268873b8b5a2cc4c8128437556262e0
Facu Lezcano <facu_lezca@hotmail.com>
2016-11-06 02:10:00 -0300

    Se agrega obstaculo Block para delimitar el area de juego.

---
+++ release/src/assets/obstaculos/Block.java
@@ -0,0 +1,26 @@
+package assets.obstaculos;
+
+import assets.Bullet;
+import assets.Obstaculo;
+import assets.Tanque;
+
+public class Block extends Obstaculo {
+	
+	
+	public Block() {
+		
+	}
+
+	@Override
+	public void colisionaBala(Bullet b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void colisionaTanque(Tanque t) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
