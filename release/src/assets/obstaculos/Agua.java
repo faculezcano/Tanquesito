b3d15f29e9ce63d195618f2f52aa081083fe6e59
Facu Lezcano <facu_lezca@hotmail.com>
2016-11-06 21:32:00 -0300

    Se le pone sprite

--- release/src/assets/obstaculos/Agua.java
+++ release/src/assets/obstaculos/Agua.java
@@ -3,11 +3,17 @@ package assets.obstaculos;
 import assets.Bullet;
 import assets.Obstaculo;
 import assets.Tanque;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 
 /**
  * 
  */
 public class Agua extends Obstaculo {
+	
+	public Agua(){
+		forma.setFill(new ImagePattern(new Image(getClass().getClassLoader().getResourceAsStream("img/agua.png"))));
+	}
 
 	@Override
 	public void colisionaBala(Bullet b) {
